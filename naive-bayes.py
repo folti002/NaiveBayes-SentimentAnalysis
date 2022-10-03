7b49890fb23076b6b89e4a4deb39e19456ac2c74
Mikkel Folting <folti002@umn.edu>
2022-10-02 23:18:47 -0500

    Calculations complete - errors though
    Values are way too low - everything is rounding to zero...

--- naive-bayes.py
+++ naive-bayes.py
@@ -8,16 +8,21 @@ from nltk.corpus import stopwords
 # nltk.download('stopwords')
 
 posTrain = {}
+posWordCount = 0
 negTrain = {}
+negWordCount = 0
 vocab = set()
-testCorpuses = []
+posTest = []
+negTest = []
+
+numPosReviews = 0
+numNegReviews = 0
 
 posDirectory = 'movie_reviews/pos'
 negDirectory = 'movie_reviews/neg'
 
 def createTrainAndTestSets(directory, filename, count, stopWords):
-  # if count > 15:
-  #   return 
+  global posWordCount, negWordCount
   f = os.path.join(directory, filename)
   file = open(f)
   corpus = file.read()
@@ -25,7 +30,7 @@ def createTrainAndTestSets(directory, filename, count, stopWords):
 
   corpus = re.sub("\n", " ", corpus) # Remove newline characters
   corpus = re.sub(r'-+', " ", corpus) # Remove any occurrences of - and replace with a space
-  corpus = re.sub(r'[\.?!,;():`"]', "", corpus) # Remove punctuation and parantheses
+  corpus = re.sub(r'[\.?!,;():`"\']', "", corpus) # Remove punctuation and parantheses
   words = list(nltk.tokenize.word_tokenize(corpus)) # Tokenize words
 
   # Filter out stop words from our list of words
@@ -36,7 +41,10 @@ def createTrainAndTestSets(directory, filename, count, stopWords):
 
   # Reserve 25% of the reviews for testing
   if count % 4 == 0:
-    testCorpuses.append(words)
+    if directory == posDirectory:
+      posTest.append(words)
+    else:
+      negTest.append(words)
     return
 
   # Add every word we see in our filtered words list to our vocab
@@ -47,12 +55,14 @@ def createTrainAndTestSets(directory, filename, count, stopWords):
     
     # Add the words into the approprate training set and increment counts
     if directory == posDirectory:
+      posWordCount = posWordCount + 1
       if w in posTrain:
         val = posTrain.get(w)
         posTrain.update({w : val + 1})
       else:
         posTrain.update({w : 1})
     else:
+      negWordCount = negWordCount + 1
       if w in negTrain:
         val = negTrain.get(w)
         negTrain.update({w : val + 1})
@@ -61,26 +71,99 @@ def createTrainAndTestSets(directory, filename, count, stopWords):
   return
 
 def createPosAndNegSets():
+  global numPosReviews, numNegReviews
   # Set of stop words to be removed from the corpus
   stopWords = set(stopwords.words('english'))
   count = 0
   for filename in os.listdir(posDirectory):
     createTrainAndTestSets(posDirectory, filename, count, stopWords)
     count = count + 1
+  numPosReviews = count
   count = 0
   for filename in os.listdir(negDirectory):
     createTrainAndTestSets(negDirectory, filename, count, stopWords)
     count = count + 1
+  numNegReviews = count
   return
 
 def testClassifiers():
+  totalPos = len(posTest)
+  totalNeg = len(negTest)
   
+  truePos = 0
+  falsePos = 0
+  for review in posTest:
+    productPos = (float(numPosReviews)) / (numPosReviews + numNegReviews)
+    productNeg = (float(numNegReviews)) / (numPosReviews + numNegReviews)
+    for w in review:
+      # FIX - ADD SMOOTHING TO PREVENT ERROR WHERE WORD DOESN'T APPEAR IN ONE OF THE TRAINING SETS
+      # if w not in vocab:
+      #   continue
+      if w not in vocab:
+        continue
+
+      posCount = 0
+      negCount = 0
+      if w in posTrain:
+        posCount = posTrain.get(w)
+      if w in negTrain:
+        negCount = negTrain.get(w)
+
+      productPos = productPos * ((posCount + 1) / (posWordCount + len(vocab))) 
+      productNeg = productNeg * ((negCount + 1) / (negWordCount + len(vocab)))
+    print(productPos, productNeg)
+    if productPos >= productNeg:
+      truePos = truePos + 1
+    else:
+      falsePos = falsePos + 1
+
+  trueNeg = 0
+  falseNeg = 0
+  for review in negTest:
+    productPos = (float(numPosReviews)) / (numPosReviews + numNegReviews)
+    productNeg = (float(numNegReviews)) / (numPosReviews + numNegReviews)
+    for w in review:
+      # FIX - ADD SMOOTHING TO PREVENT ERROR WHERE WORD DOESN'T APPEAR IN ONE OF THE TRAINING SETS
+      # if w not in vocab:
+      #   continue
+
+      if w not in vocab:
+        continue
+
+      posCount = 0
+      negCount = 0
+      if w in posTrain:
+        posCount = posTrain.get(w)
+      if w in negTrain:
+        negCount = negTrain.get(w)
+
+      productPos = productPos * ((posCount + 1) / (posWordCount + len(vocab))) 
+      productNeg = productNeg * ((negCount + 1) / (negWordCount + len(vocab)))
+    if productPos > productNeg:
+      falseNeg = falseNeg + 1
+    else:
+      trueNeg = trueNeg + 1
+
+  print(str(truePos) + "\t" + str(falsePos))
+  print(str(falseNeg) + "\t" + str(trueNeg))
+
+  precision = truePos / (truePos + falsePos)
+  precisionPercentage = "{:.1%}".format((precision))
+  recall = truePos / (truePos + falseNeg)
+  recallPercentage = "{:.1%}".format((recall))
+  fscore = (2 * precision * recall) / (precision + recall)
+  print("Precision:\t", precisionPercentage)
+  print("Recall: \t", recallPercentage)
+  print("F-Score:\t", fscore)
 
 def main():
   # First we will create the sets of counts for positive and negative reviews
+  print("Creating training and test sets...")
   createPosAndNegSets()
-
+  print("Positive word count:", posWordCount)
+  print("Negative word count:", negWordCount)
   # Now that we have the sets created, we want to start looking at the test sets to find probabilities
+  print("Testing our classifiers...")
   testClassifiers()
   return
 
